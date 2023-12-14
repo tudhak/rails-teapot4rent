d8902d841e7c188b7bd29f29f8d4ceda8a3a448b
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-12-14 13:46:37 +0100

    model validations added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :teapot
   belongs_to :user
+
+  validates :start_date, :end_date, :status, presence: true
+  validates :total_price, presence: true, numericality: true
 end
--- app/models/teapot.rb
+++ app/models/teapot.rb
@@ -1,3 +1,8 @@
 class Teapot < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+
+  validates :style, presence: true, length: { minimum: 6 }
+  validates :description, presence: true, length: { minimum: 20 }
+  validates :price_per_day, presence: true, numericality: true
 end
