cb4a73fbe666e30aa77c8be3b0796ccc1b05a610
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-12-19 10:37:40 +0100

    favicon added

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 
 export default class extends Controller {
+
   connect() {
     flatpickr(this.element, {
       minDate: "today"
